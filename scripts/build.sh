d5e88782ade6147394acf73e8e779e470a54ecb2
Aliaksandr Kavalchuk <avkavalchuk@gmail.com>
2022-11-20 16:30:03 +0300

    Create build.sh

---
+++ scripts/build.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+revanced=no
+while getopts mr flag
+do
+    case "${flag}" in
+        r) revanced=yes;;
+    esac
+done
+
+VMG_VERSION="0.2.24.220220"
+
+# Artifacts associative array aka dictionary
+declare -A artifacts
+
+artifacts["revanced-cli.jar"]="revanced/revanced-cli revanced-cli .jar"
+artifacts["revanced-integrations.apk"]="revanced/revanced-integrations app-release-unsigned .apk"
+artifacts["revanced-patches.jar"]="revanced/revanced-patches revanced-patches-2.114.0 .jar"
+artifacts["apkeep"]="EFForg/apkeep apkeep-x86_64-unknown-linux-gnu"
+
+get_artifact_download_url () {
+    # Usage: get_download_url <repo_name> <artifact_name> <file_type>
+    local api_url="https://api.github.com/repos/$1/releases/latest"
+    local result=$(curl $api_url | jq ".assets[] | select(.name | contains(\"$2\") and contains(\"$3\") and (contains(\".sig\") | not)) | .browser_download_url")
+    echo ${result:1:-1}
+}
+
+# Fetch all the dependencies
+for artifact in "${!artifacts[@]}"; do
+    if [ ! -f $artifact ]; then
+        echo "Downloading $artifact"
+        curl -L -o $artifact $(get_artifact_download_url ${artifacts[$artifact]})
+    fi
+done
+
+# Fetch Vanced microG
+chmod +x apkeep
+
+if [ "$revanced" = 'yes' ]; then
+    if [ ! -f "vanced-microG.apk" ]; then
+        echo "Downloading Vanced microG"
+        ./apkeep -a com.mgoogle.android.gms@$VMG_VERSION .
+        mv com.mgoogle.android.gms@$VMG_VERSION.apk vanced-microG.apk
+    fi
+fi
+
+mkdir -p build
+
+# A list of available patches and their descriptions can be found here:
+# https://github.com/LeddaZ/revanced-patches
+
+if [ "$revanced" = 'yes' ]; then
+    echo "************************************"
+    echo "*    Building YouTube ReVanced     *"
+    echo "************************************"
+
+    yt_excluded_patches="-e background-play -e codecs-unlock -e compact-header -e custom-music-branding -e exclusive-audio-playback -e hide-get-premium -e minimized-playback-music -e music-microg-support -e music-video-ads -e premium-heading -e tasteBuilder-remover -e upgrade-button-remover"
+
+    if [ -f "youtube.apk" ]; then
+        java -jar revanced-cli.jar -m revanced-integrations.apk -b revanced-patches.jar \
+                                $yt_excluded_patches \
+                                -a youtube.apk -o build/revanced-nonroot.apk
+        echo "YouTube ReVanced build finished"
+    else
+        echo "Cannot find YouTube APK, skipping build"
+    fi
+else
+    echo "Skipping YouTube Revanced build"
+fi
