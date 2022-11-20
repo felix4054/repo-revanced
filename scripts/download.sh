91915cf82967a9dff45c1f2440960515b9438b3d
Aliaksandr Kavalchuk <avkavalchuk@gmail.com>
2022-11-20 16:32:54 +0300

    Create download.sh

---
+++ scripts/download.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+revanced=no
+while getopts mr flag
+do
+    case "${flag}" in
+        r) revanced=yes;;
+    esac
+done
+
+YT_VERSION="17-43-36"
+
+declare -A apks
+
+apks["youtube.apk"]=dl_yt
+
+## Functions
+
+# Wget user agent
+WGET_HEADER="User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0"
+
+# Wget function
+req() { wget -nv -O "$2" --header="$WGET_HEADER" "$1"; }
+
+# Wget download apk
+dl_apk() {
+	local url=$1 regexp=$2 output=$3
+	url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n "s/href=\"/@/g; s;.*${regexp}.*;\1;p")"
+	echo "$url"
+	url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n 's;.*href="\(.*key=[^"]*\)">.*;\1;p')"
+	url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n 's;.*href="\(.*key=[^"]*\)">.*;\1;p')"
+	req "$url" "$output"
+}
+
+# Download YouTube
+dl_yt() {
+	if [ "$revanced" = 'yes' ]; then
+		echo "Downloading YouTube..."
+
+		local base_apk="youtube.apk"
+		if [ ! -f "$base_apk" ]; then
+			declare -r dl_url=$(dl_apk "https://www.apkmirror.com/apk/google-inc/youtube/youtube-${YT_VERSION}-release/" \
+				"APK</span>[^@]*@\([^#]*\)" \
+				"$base_apk")
+		fi
+	else
+		echo "Skipping YouTube..."
+	fi
+}
+
+## Main
+
+for apk in "${!apks[@]}"; do
+    if [ ! -f $apk ]; then
+        ${apks[$apk]}
+    fi
+done
