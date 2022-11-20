e8161837d7fc50b6708909b8bfb1a1d82f82a71c
felix4054 <avkavalchuk@gmail.com>
2022-11-20 19:45:09 +0300

    Update patchers

--- scripts/build.sh
+++ scripts/build.sh
@@ -57,7 +57,7 @@ if [ "$revanced" = 'yes' ]; then
     yt_excluded_patches="-e background-play -e codecs-unlock -e compact-header -e custom-music-branding -e exclusive-audio-playback -e hide-get-premium -e minimized-playback-music -e music-microg-support -e music-video-ads -e premium-heading -e tasteBuilder-remover -e upgrade-button-remover"
 
     if [ -f "youtube.apk" ]; then
-        java -jar revanced-cli.jar -m revanced-integrations.apk -b revanced-patches.jar \
+        java -jar revanced-cli.jar -m revanced-integrations.apk -b revanced-patches-2.114.0.jar \
                                 $yt_excluded_patches \
                                 -a youtube.apk -o build/revanced-nonroot.apk
         echo "YouTube ReVanced build finished"
