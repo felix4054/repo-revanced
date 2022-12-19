34a2c9ade5b49cfef65a1fc1a3531c501e6ad537
Aliaksandr Kavalchuk <avkavalchuk@gmail.com>
2022-12-19 20:07:05 +0300

    Update sha256gen.sh

--- scripts/sha256gen.sh
+++ scripts/sha256gen.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-revanced=no
-while getopts r flag
-do
-    case "${flag}" in
-        r) revanced=yes;;
-    esac
-done
-
 # Generate SHA-256 hashes
-if [ "$revanced" = 'yes' ]; then
-    sha256sum build/revanced-nonroot-release.apk > build/SHA-256-yt.txt
-fi
+
+sha256sum build/revanced-nonroot-release.apk > build/SHA-256-yt.txt
+
