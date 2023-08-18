a7636c7620a43277b01b2756f342a6824073c874
Aliaksandr Kavalchuk <avkavalchuk@gmail.com>
2023-08-18 23:59:20 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -41,7 +41,7 @@ declare -A artifacts
 # artifacts["microg.apk"]="inotia00/VancedMicroG microg .apk"
 # artifacts["apkeep"]="EFForg/apkeep apkeep-x86_64-unknown-linux-gnu"
 
-artifacts["revanced-cli.jar"]="revanced/revanced-cli revanced-cli*-all.jar"
+artifacts["revanced-cli.jar"]="revanced/revanced-cli revanced-cli*.jar"
 artifacts["revanced-integrations.apk"]="YT-Advanced/ReX-integrations revanced-integrations*.apk"
 artifacts["revanced-patches.jar"]="YT-Advanced/ReX-patches revanced-patches*.jar"
 artifacts["microg.apk"]="inotia00/VancedMicroG microg*.apk"
