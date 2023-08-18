a355187bf7227d34c57867aa290190a29cfd5bd3
Aliaksandr Kavalchuk <avkavalchuk@gmail.com>
2023-08-19 00:47:34 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -35,17 +35,17 @@ declare -a patches
 # Artifacts associative array aka dictionary
 declare -A artifacts
 
-# artifacts["revanced-cli-all.jar"]="inotia00/revanced-cli revanced-cli .jar"
-# artifacts["revanced-integrations.apk"]="inotia00/revanced-integrations revanced-integrations .apk"
-# artifacts["revanced-patches.jar"]="inotia00/revanced-patches revanced-patches .jar"
-# artifacts["microg.apk"]="inotia00/VancedMicroG microg .apk"
-# artifacts["apkeep"]="EFForg/apkeep apkeep-x86_64-unknown-linux-gnu"
-
-artifacts["revanced-cli.jar"]="revanced/revanced-cli revanced-cli .jar"
-artifacts["revanced-integrations.apk"]="YT-Advanced/ReX-integrations revanced-integrations .apk"
-artifacts["revanced-patches.jar"]="YT-Advanced/ReX-patches revanced-patches .jar"
+artifacts["revanced-cli.jar"]="inotia00/revanced-cli revanced-cli .jar"
+artifacts["revanced-integrations.apk"]="inotia00/revanced-integrations revanced-integrations .apk"
+artifacts["revanced-patches.jar"]="inotia00/revanced-patches revanced-patches .jar"
 artifacts["microg.apk"]="inotia00/VancedMicroG microg .apk"
 artifacts["apkeep"]="EFForg/apkeep apkeep-x86_64-unknown-linux-gnu"
+
+#artifacts["revanced-cli.jar"]="revanced/revanced-cli revanced-cli .jar"
+#artifacts["revanced-integrations.apk"]="YT-Advanced/ReX-integrations revanced-integrations .apk"
+#artifacts["revanced-patches.jar"]="YT-Advanced/ReX-patches revanced-patches .jar"
+#artifacts["microg.apk"]="inotia00/VancedMicroG microg .apk"
+#artifacts["apkeep"]="EFForg/apkeep apkeep-x86_64-unknown-linux-gnu"
 
 # artifacts["revanced-cli.jar"]="revanced/revanced-cli revanced-cli .jar"
 # artifacts["revanced-integrations.apk"]="revanced/revanced-integrations app-release-unsigned .apk"
