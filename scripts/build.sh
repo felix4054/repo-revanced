5bd3135b3092e8f6c839d8c0a4aa17c8479f0812
Aliaksandr Kavalchuk <avkavalchuk@gmail.com>
2023-09-12 18:33:39 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -155,7 +155,7 @@ if [ -f "com.google.android.youtube.apk" ]; then
          -e microg-support \
 	 ${patches[@]} \
          # $EXPERIMENTAL \
-         -a com.google.android.youtube.apk -o "build/revanced-youtube-$(cat versions.json | grep -oP '(?<="com.google.android.youtube.apk": ")[^"]*')-root.apk"
+         com.google.android.youtube.apk -o "build/revanced-youtube-$(cat versions.json | grep -oP '(?<="com.google.android.youtube.apk": ")[^"]*')-root.apk"
 else
     out "${RED}Cannot find YouTube APK, skipping build"
 fi
@@ -166,10 +166,11 @@ out "${YELLOW}Building Non-root APK"
 
 if [ -f "com.google.android.youtube.apk" ]; then
     java -jar revanced-cli.jar -m revanced-integrations.apk -b revanced-patches.jar patch \
+    	 com.google.android.youtube.apk \
 	 -i microg-support \
          ${patches[@]} \
          # $EXPERIMENTAL \
-         -a com.google.android.youtube.apk -o com.google.android.youtube.apk "build/revanced-youtube-$(cat versions.json | grep -oP '(?<="com.google.android.youtube.apk": ")[^"]*').apk"
+         -o "build/revanced-youtube-$(cat versions.json | grep -oP '(?<="com.google.android.youtube.apk": ")[^"]*').apk"
 else
     out "${RED}Cannot find YouTube APK, skipping build"
 fi
